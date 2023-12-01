4678e8f8b31955fa2ca8b58c45032f35560a92d5
RieBi <42877671+RieBi@users.noreply.github.com>
2023-12-01 22:30:09 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using AdventOfCode.Year2023;
+
+namespace AdventOfCode;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Solve<Day1>();
+    }
+
+    static void Solve<T>() where T : Day, new()
+    {
+        var day = new T();
+        var path = @$".\Year2023\Input\Day{day.DayNumber}.txt";
+        var input = File.ReadAllText(path);
+        day.Input = input;
+
+        Console.WriteLine("Part one:");
+        try
+        {
+            day.PartOne();
+        }
+        catch
+        {
+            Console.WriteLine("Catched an exception");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Part two:");
+        try
+        {
+            day.PartTwo();
+        }
+        catch
+        {
+            Console.WriteLine("Catched an exception");
+        }
+    }
+}
+
+public abstract class Day
+{
+    public string Input { get; set; } = default!;
+    public int DayNumber { get; set; } = default!;
+
+    public virtual void PartOne() { }
+
+    public virtual void PartTwo() { }
+}
---
+++ Year2023/Day1.cs
@@ -0,0 +1,66 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Text.RegularExpressions;
+namespace AdventOfCode.Year2023;
+internal class Day1 : Day
+{
+    private char[] numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+	public Day1()
+	{
+		this.DayNumber = 1;
+	}
+
+    public override void PartOne()
+    {
+        var sum = 0;
+        foreach (var line in Input.Split('\n'))
+        {
+            var firstNumber = line.First(f => numbers.Contains(f)).ToString();
+            var secondNumber = line.Last(f => numbers.Contains(f)).ToString();
+            var combinedInt = int.Parse(firstNumber + secondNumber);
+            sum += combinedInt;
+        }
+
+        Console.WriteLine(sum);
+    }
+
+    public override void PartTwo()
+    {
+        var pattern = @"(?=(one|two|three|four|five|six|seven|eight|nine|\d))";
+        var sum = 0;
+
+        var transform = new Dictionary<string, string>
+        {
+            { "one", "1" },
+            { "two", "2" },
+            { "three", "3" },
+            { "four", "4" },
+            { "five", "5" },
+            { "six", "6" },
+            { "seven", "7" },
+            { "eight", "8" },
+            { "nine", "9" },
+            { "1", "1" },
+            { "2", "2" },
+            { "3", "3" },
+            { "4", "4" },
+            { "5", "5" },
+            { "6", "6" },
+            { "7", "7" },
+            { "8", "8" },
+            { "9", "9" }
+        };
+
+        foreach (var line in Input.Split('\n'))
+        {
+            var matches = Regex.Matches(line, pattern);
+            var firstNumber = transform[matches.First().Groups[1].Value];
+            var secondNumber = transform[matches.Last().Groups[1].Value];
+            var combined = firstNumber + secondNumber;
+            sum += int.Parse(combined);
+        }
+
+        Console.WriteLine(sum);
+    }
+}
